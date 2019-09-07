08af69be89b352a2bcf4d4d3cfb40d7d16999f08
Andrew Olney <aolney@memphis.edu>
2019-09-07 15:28:18 -0500

    attempting to fix sassc build problem 2

--- build_sassc.sh
+++ build_sassc.sh
@@ -6,6 +6,7 @@ git clone https://github.com/sass/sassc.git
 #choose last building commit
 cd sassc
 git reset --hard 3f84e2358019dab2fdba5fe1fc0ecff23aa24608
+cd ..
 #end choose last building commit
 . sassc/script/bootstrap
 make -C sassc -j4
